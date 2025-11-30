6810d9cadf27074542ac6183ed48f405e826b744
LuigiGarcia03 <luisitoma99garcia@gmail.com>
2025-11-30 16:49:12 -0600

    Se elimino la importacion de FiDollarSign en About.tsx

--- web-app/src/components/About/About.tsx
+++ web-app/src/components/About/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './About.module.css';
 import { 
-  FiTarget, FiEye, FiActivity, FiDollarSign, 
+  FiTarget, FiEye, FiActivity,
   FiShield, FiTrendingUp, FiUsers, FiAward, 
   FiBox, FiGlobe, 
   FiBookOpen,
